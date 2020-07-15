f8d114c6d6d670c1cdff1046c028e87b0e8fbd47
Ohkura Tessyuu <ito1002jp@gmail.com>
2020-07-15 20:22:34 +0900

    first commit

---
+++ src/cs1102/SuperPower.java
@@ -0,0 +1,11 @@
+package cs1102;
+
+import javax.swing.JOptionPane;
+
+public class SuperPower {
+	public static void main(String[] args)
+	{
+		String answer = JOptionPane.showInputDialog("eay");
+		System.out.println(answer);
+	} 
+}
